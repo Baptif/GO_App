8005df8388694544017e8fa29e01e234c5daaebf
Baptiste Delhomme <baptdelhomme@gmail.com>
2023-11-22 23:37:25 +0100

    :boom: Ajout de la création de tâches + liste des tâches + ajout d'effectuer la tâche

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,14 @@
-import { Box } from '@mantine/core'
+import { Box, List, ThemeIcon } from '@mantine/core'
 import useSWR from 'swr'
 import AddTodo from './components/AddTodo'
+import { CheckCircleFillIcon } from '@primer/octicons-react';
+
+export interface Todo {
+  id: number
+  title: string
+  body: string
+  done: boolean
+}
 
 export const ENDPOINT = 'http://localhost:4000'
 
@@ -9,11 +17,48 @@ const fetcher = (url: string) =>
 
 const App = () => {
 
-  const { data, mutate } = useSWR('api/todos', fetcher)
+  const { data, mutate } = useSWR<Todo[]>('api/todos', fetcher)
+
+  const markTodoAsDone = async (id: number) => {
+    const updated = await fetch(`${ENDPOINT}/api/todos/${id}/done`, {
+      method: "PATCH",
+    }).then((res) => res.json())
+
+    mutate(updated)
+  }
 
   return (
-    <Box>{JSON.stringify(data)}
-      <AddTodo/>
+    <Box
+      style={{
+        padding: "2rem",
+        width: "100%",
+        maxWidth: "40rem",
+        margin: "0 auto",
+      }}
+    >
+      <List spacing="xs" size="sm" mb={12} center>
+        {data?.map((todo) => {
+          return (
+            <List.Item 
+              onClick={() => markTodoAsDone(todo.id)}
+              key={`todo_list_${todo.id}`}
+              icon={
+                todo.done ? (
+                  <ThemeIcon color="teal" size={24} radius="xl">
+                    <CheckCircleFillIcon size={20} />
+                  </ThemeIcon>
+                ) : (
+                  <ThemeIcon color="gray" size={24} radius="xl">
+                    <CheckCircleFillIcon size={20} />
+                  </ThemeIcon>
+                )
+              }
+            >{todo.title}</List.Item>
+          )
+        })}
+      </List>
+
+      <AddTodo mutate={mutate}/>
     </Box>
     
   )
--- client/src/components/AddTodo.tsx
+++ client/src/components/AddTodo.tsx
@@ -1,8 +1,10 @@
 import {useState} from 'react'
 import {useForm} from '@mantine/form'
-import { Button, Group, Modal } from '@mantine/core'
+import { Button, Group, Modal, TextInput, Textarea } from '@mantine/core'
+import { ENDPOINT, Todo } from '../App'
+import { KeyedMutator } from 'swr'
 
-const AddTodo = () => { 
+const AddTodo = ({mutate} : {mutate: KeyedMutator<Todo[]>}) => { 
     const [open, setOpen] = useState(false)
 
     const form = useForm({
@@ -12,10 +14,40 @@ const AddTodo = () => {
         },
     })
 
+    const createTodo = async (values: {title: string, body: string}) => {
+        const updated = await fetch(`${ENDPOINT}/api/todos`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(values),
+        }).then((res) => res.json())
+
+        mutate(updated)
+        form.reset()
+        setOpen(false)
+    }
+
     return (
         <>
         <Modal opened={open} onClose={() => setOpen(false)} title="Create todo">
-            Text
+            <form onSubmit={form.onSubmit(createTodo)}>
+                <TextInput
+                    required
+                    mb={12}
+                    label="Todo"
+                    placeholder="What do you need to do ?"
+                    {...form.getInputProps("title")}
+                />
+                <Textarea
+                    required
+                    mb={12}
+                    label="Body"
+                    placeholder="Tell me more about it.."
+                    {...form.getInputProps("body")}
+                />
+                <Button type="submit">Create TODO</Button>
+            </form>
         </Modal>
         <Group>
             <Button fullWidth mb={12} onClick={() => setOpen(true)}>ADD TODO</Button>
